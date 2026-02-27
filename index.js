6d38940288cebdb56a8dcd971fe6bdf1a36dd150
moizxox <mianmoiz899@gmail.com>
2026-02-27 11:45:09 +0500

    scoket update

--- index.js
+++ index.js
@@ -54,6 +54,7 @@ io.on("connection", (socket) => {
   socket.on("concert-update", (data) => io.emit("concert-update-res", data));
   socket.on("credits-info-req", (data) => io.emit("credits-info-res", data));
   socket.on("test-req", (data) => io.emit("test-res", data));
+  socket.on("send-admin-reg-req", (data) => io.emit("send-admin-reg-res", data));
 });
 
 app.get("/", (req, res) => {
